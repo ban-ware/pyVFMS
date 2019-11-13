41739a675a72fe3a7b3a709343f951d881d9a50e
ban-ware <43831016+ban-ware@users.noreply.github.com>
2019-11-13 16:55:33 -0600

    Update login.py
    Added support for esp8266 and additional authentication to accounts by comparison to logged in linux user.  If accounts do not match, access is denied.

--- login.py
+++ login.py
@@ -2,8 +2,19 @@
 import sys; print(sys.version) 	#display python version in use on launch
 import hashlib, binascii, os	#hashlib for secure hashing, binascii for hash encoding os urandom function for 				creating a random number of size n bytes
 import pyfirmata
-#board = Arduino('/dev/tty0') # change to name of in use serial port
-#board.digital[15].write(1) #Test line to turn on an LED if this program is interfacing with device
+import getpass # to get information on currently logged in user on linux machine
+import subprocess
+
+#	pinout guide:
+#	NodeMCU pin:	Arduino Pin: (Write to arduino pin)
+#		4		2	red
+#		6		12	green
+#		7		13	blue
+		
+board = pyfirmata.Arduino('/dev/ttyUSB0')
+#board.digital[2].write(1)   For testing purposes
+#board.digital[12].write(1)
+#board.digital[13].write(1)
 
 def signup(choice):
 	#open the credentials file for appending, this allows us to store multiple username and password combos
@@ -47,9 +58,20 @@ def login(choice):
 		if(hpassword in passwords):
 			#print("Your password is in the file")
 			print("You are now logged into the system")
+			return (logged_in = 1)
 			if (username not in credentials and hpassword not in passwords):
 				print("Username and password combination not found, closing...")
-
+				return(logged_in = 0)
+def usercheck(logged_in):
+	if (logged_in = 1):
+		fd = open("credentials.txt", "r")
+		credentials = fd.read()
+		ux_username = getpass.getuser()
+		if (ux_username not in credentials):
+			print("Current user not authorized to access files on this account.")
+			exit()
+		elif(ux_username in credentials):
+			print("Current user is authorized to access files on this account.")
 
 def hashpassword(password):
 	#function for creating the password hash
@@ -61,15 +83,19 @@ def hashpassword(password):
 
 def main():
 	#global username, password
+	logged_in = 0
 	choice = raw_input('Enter 1 to Sign up, 2 to Login  ')
 	choice = int(choice)
-	while (choice is not 1 or 2):
-		if (choice == 1):
-			signup(choice)
-		elif (choice == 2):
+	
+	if (choice == 1):
+		signup(choice)
+	while (logged_in is not 1):
+		if (choice == 2):
 			login(choice)
 		else:
 			print("Please choose between options 1 and 2.")
+	
+	
 		
 main()
 
